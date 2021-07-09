7ff7efbc77475483409037fe3ebdf7c87db49e6b
Si Chen <sichen83.us@gmail.com>
2021-07-08 19:31:34 -0700

    first react JSX

---
+++ MERN_doc.js
@@ -0,0 +1,31 @@
+****************************************************************************************
+--Class video：
+https://www.youtube.com/channel/UCCBf2vuTbk8sD5tiAPwKEkQ
+
+--Getting started Webpack
+https://webpack.js.org/guides/getting-started/
+
+--Introducing JSX
+https://reactjs.org/docs/introducing-jsx.html
+
+--React DOM:
+https://reactjs.org/docs/dom-elements.html
+
+--Function and Class Components (React)
+https://reactjs.org/docs/components-and-props.html#function-and-class-components
+
+--SyntheticEvent(React)
+https://reactjs.org/docs/events.html#supported-events
+
+--Handling Events
+https://reactjs.org/docs/handling-events.html
+
+--Introducing Hooks
+https://reactjs.org/docs/hooks-intro.html
+
+--Uncontrolled Components
+https://reactjs.org/docs/uncontrolled-components.html
+
+
+
+*****************************************************************************************
