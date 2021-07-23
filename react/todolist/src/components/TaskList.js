aa79471ed1dea647d85c86bfb98cd0d37361314a
Si Chen <sichen83.us@gmail.com>
2021-07-23 15:34:24 -0700

    todolist update

--- react/todolist/src/components/TaskList.js
+++ react/todolist/src/components/TaskList.js
@@ -3,8 +3,9 @@ import styles from './toDoList.module.css';
 const TaskList = (props)=>{
     const {taskArr, handleDelete, handleCheck, handleUnCheck} = props;
     const handleChange = (e,index)=>{
-        e.preventDefault();
-        taskArr[index].completed? handleUnCheck(index): handleCheck(index)
+        //e.preventDefault();
+        taskArr[index].completed? handleUnCheck(index): handleCheck(index);
+        console.log(taskArr);
     }
     return(
         <div className={styles.taskList}>
@@ -13,7 +14,7 @@ const TaskList = (props)=>{
                 {
                     taskArr.map((item, index)=>{
                         return (
-                            <li className={styles.taskItem}key={index}>
+                            <li className={styles.taskItem} key={index}>
                                 <span className={item.completed? styles.checked: ""}>{item.content}</span>
                                 <input type="checkbox" checked={item.completed} onChange={(e)=>handleChange(e,index)}/>
                                 <button className={styles.deleteBtn} onClick={(e)=>handleDelete(e,index)}>Delete</button>
